8fe0441e2ed21937dce183c88f910e846c23cc21
Alex Mikitsik <mikitsik@gmail.com>
2026-04-11 11:08:15 +0300

    Remove minitests. Add RSpec

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,20 @@ module Doorsite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: true,
+        system_specs: false
+
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+
+      g.system_tests nil
+    end
   end
 end
