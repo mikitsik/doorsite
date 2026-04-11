693f864c9fb845ed8026602bf6a18dc5d791f6df
Alex Mikitsik <mikitsik@gmail.com>
2026-04-11 18:19:23 +0300

    Prepare for Render

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,50 @@
+# require "csv"
+
+# Product.delete_all
+
+# csv_path = Rails.root.join("db/products.csv")
+
+# CSV.foreach(csv_path, headers: true) do |row|
+#   Product.create!(
+#     slug: row["slug"],
+#     title: row["title"],
+#     brand: row["brand"],
+#     category: row["category"],
+#     price: row["price"],
+#     currency: row["currency"],
+#     image_url: row["image_url"],
+#     description: row["description"],
+#     active: row["active"] == "true"
+#   )
+# end
+
+# puts "Created #{Product.count} products"
+
 require "csv"
+require "base64"
 
 Product.delete_all
 
-csv_path = Rails.root.join("db/products.csv")
+if ENV["PRODUCTS_CSV_BASE64"].present?
+  decoded = Base64.decode64(ENV["PRODUCTS_CSV_BASE64"])
 
-CSV.foreach(csv_path, headers: true) do |row|
-  Product.create!(
-    slug: row["slug"],
-    title: row["title"],
-    brand: row["brand"],
-    category: row["category"],
-    price: row["price"],
-    currency: row["currency"],
-    image_url: row["image_url"],
-    description: row["description"],
-    active: row["active"] == "true"
-  )
+  CSV.parse(decoded, headers: true) do |row|
+    Product.create!(
+      slug: row["slug"],
+      title: row["title"],
+      brand: row["brand"],
+      category: row["category"],
+      price: row["price"],
+      currency: row["currency"],
+      image_url: row["image_url"],
+      description: row["description"],
+      active: row["active"] == "true"
+    )
+  end
+
+  puts "Loaded products from ENV"
+else
+  puts "No CSV provided"
 end
 
-puts "Created #{Product.count} products"
+puts "Total: #{Product.count}"
