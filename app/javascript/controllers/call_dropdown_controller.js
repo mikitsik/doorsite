122130a4402e248562bcbdec4afde674ee269263
Alex Mikitsik <mikitsik@gmail.com>
2026-04-20 22:24:36 +0300

    PreFinal UI polishing

--- app/javascript/controllers/call_dropdown_controller.js
+++ app/javascript/controllers/call_dropdown_controller.js
@@ -3,23 +3,51 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["menu"]
 
-  toggle(event) {
-    event.stopPropagation()
-    this.element.classList.toggle("is-open")
-  }
-
   connect() {
-    this.handleOutsideClick = this.handleOutsideClick.bind(this)
-    document.addEventListener("click", this.handleOutsideClick)
+    this.handleDocumentClick = this.handleDocumentClick.bind(this)
+    document.addEventListener("click", this.handleDocumentClick)
   }
 
   disconnect() {
-    document.removeEventListener("click", this.handleOutsideClick)
+    document.removeEventListener("click", this.handleDocumentClick)
   }
 
-  handleOutsideClick(event) {
-    if (!this.element.contains(event.target)) {
-      this.element.classList.remove("is-open")
+  toggle(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const isOpen = this.element.classList.contains("is-open")
+
+    this.closeAll()
+
+    if (!isOpen) {
+      this.element.classList.add("is-open")
+      this.setExpanded(true)
     }
+  }
+
+  handleDocumentClick(event) {
+    if (!this.element.contains(event.target)) {
+      this.close()
+    }
+  }
+
+  close() {
+    this.element.classList.remove("is-open")
+    this.setExpanded(false)
+  }
+
+  closeAll() {
+    document.querySelectorAll(".call-dropdown.is-open").forEach((dropdown) => {
+      dropdown.classList.remove("is-open")
+
+      const button = dropdown.querySelector(".call-dropdown__toggle")
+      if (button) button.setAttribute("aria-expanded", "false")
+    })
+  }
+
+  setExpanded(value) {
+    const button = this.element.querySelector(".call-dropdown__toggle")
+    if (button) button.setAttribute("aria-expanded", String(value))
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,6 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import CallDropdownController from "./call_dropdown_controller"
+import CallDropdownController from "controllers/call_dropdown_controller"
 
 eagerLoadControllersFrom("controllers", application)
 application.register("call-dropdown", CallDropdownController)
