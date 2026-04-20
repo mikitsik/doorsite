a2bbdecf8800f947382712f8fe9dada50451f43d
Alex Mikitsik <mikitsik@gmail.com>
2026-04-20 21:54:04 +0300

    Dropdown

--- app/javascript/controllers/call_dropdown_controller.js
+++ app/javascript/controllers/call_dropdown_controller.js
@@ -1,42 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.handleOutsideClick = this.handleOutsideClick.bind(this)
-  }
+  static targets = ["menu"]
 
   toggle(event) {
-    event.preventDefault()
     event.stopPropagation()
-
-    const isOpen = this.element.classList.contains("is-open")
-
-    this.closeAll()
-
-    if (!isOpen) {
-      this.element.classList.add("is-open")
-      document.addEventListener("click", this.handleOutsideClick)
-    }
+    this.element.classList.toggle("is-open")
   }
 
-  close() {
-    this.element.classList.remove("is-open")
-    document.removeEventListener("click", this.handleOutsideClick)
-  }
-
-  closeAll() {
-    document.querySelectorAll(".call-dropdown.is-open").forEach((dropdown) => {
-      dropdown.classList.remove("is-open")
-    })
-  }
-
-  handleOutsideClick(event) {
-    if (!this.element.contains(event.target)) {
-      this.close()
-    }
+  connect() {
+    this.handleOutsideClick = this.handleOutsideClick.bind(this)
+    document.addEventListener("click", this.handleOutsideClick)
   }
 
   disconnect() {
     document.removeEventListener("click", this.handleOutsideClick)
+  }
+
+  handleOutsideClick(event) {
+    if (!this.element.contains(event.target)) {
+      this.element.classList.remove("is-open")
+    }
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import CallDropdownController from "./call_dropdown_controller"
 
 eagerLoadControllersFrom("controllers", application)
+application.register("call-dropdown", CallDropdownController)
