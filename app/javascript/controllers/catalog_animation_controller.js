76c4e3238c103d0640d66963e5438cf63add8240
Alex Mikitsik <mikitsik@gmail.com>
2026-04-26 13:09:54 +0300

    Add temp svg-animation

---
+++ app/javascript/controllers/catalog_animation_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["layer"]
+
+  connect() {
+    this.duration = 1800
+  }
+
+  play(event) {
+    event.preventDefault()
+
+    const el = this.layerTarget
+
+    const x = event.clientX
+    const y = event.clientY
+
+    el.style.transformOrigin = `${x}px ${y}px`
+
+    el.classList.remove("is-active")
+    void el.offsetWidth
+
+    el.classList.add("is-active")
+
+    clearTimeout(this.timer)
+    this.timer = setTimeout(() => {
+      el.classList.remove("is-active")
+    }, this.duration)
+  }
+}
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,8 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import CallDropdownController from "controllers/call_dropdown_controller"
+import CatalogAnimationController from "controllers/catalog_animation_controller"
 
 eagerLoadControllersFrom("controllers", application)
 application.register("call-dropdown", CallDropdownController)
+application.register("catalog-animation", CatalogAnimationController)
