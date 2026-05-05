0c56c74dfb93d0aff8e5d1ba310fbcdbced96f04
Alex Mikitsik <mikitsik@gmail.com>
2026-05-05 19:54:45 +0300

    Remove catalog-animation-layer

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,6 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import CallDropdownController from "controllers/call_dropdown_controller"
-import CatalogAnimationController from "controllers/catalog_animation_controller"
 
 eagerLoadControllersFrom("controllers", application)
 application.register("call-dropdown", CallDropdownController)
-application.register("catalog-animation", CatalogAnimationController)
