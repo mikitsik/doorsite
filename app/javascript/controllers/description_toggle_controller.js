d769403b063f83751ded8d6fe78193f85a9ef67e
Alex Mikitsik <mikitsik@gmail.com>
2026-05-10 20:59:00 +0300

    Added toggle description button

---
+++ app/javascript/controllers/description_toggle_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["content", "button"]
+  static classes = ["expanded"]
+
+  connect() {
+    this.updateVisibility()
+    this.updateButton()
+  }
+
+  toggle() {
+    this.element.classList.toggle(this.expandedClass)
+    this.updateButton()
+  }
+
+  updateVisibility() {
+    const collapsible = this.contentTarget.scrollHeight > this.contentTarget.clientHeight + 1
+
+    this.element.classList.toggle("is-collapsible", collapsible)
+    this.buttonTarget.hidden = !collapsible
+  }
+
+  updateButton() {
+    const expanded = this.element.classList.contains(this.expandedClass)
+
+    this.buttonTarget.textContent = expanded ? "Показать меньше" : "Показать больше"
+    this.buttonTarget.setAttribute("aria-expanded", String(expanded))
+  }
+}
