aa3e812e752eecb609c28615d0a0be33afd20027
Alex Mikitsik <mikitsik@gmail.com>
2026-05-12 13:25:29 +0300

    Styling filter button and main filters

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["panel"]
+  static targets = ["panel", "button"]
 
   toggle() {
     this.panelTarget.classList.toggle("is-open")
+    this.buttonTarget.classList.toggle("is-open")
   }
 }
